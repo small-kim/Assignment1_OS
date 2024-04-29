a12291f3334f8da5e17b244cce93d1c24f1b281c
So Hyoung Kim <iwawy1537@gmail.com>
2024-04-29 22:03:21 +0900

    final_commit

--- Assignment1_OS/main.c
+++ Assignment1_OS/main.c
@@ -1,156 +1,172 @@
-#define HAVE_STRUCT_TIMESPEC
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <time.h>
+#include <windows.h>
+#include <float.h>
 
-#define NUM_THREADS 4
-#define TIME_QUANTUM 2
+#define MAX_THREADS 4
+#define TIME_SLICE 2.0f
 
 typedef struct {
-	int thread_id;
-	int arrival_time;
-	int burst_time;
-	int remaining_time;
-} ThreadArgs;
+    int thread_id;
+    float arrival_time;
+    float execution_time;
+    float remaining_time;
+} Thread;
 
-int compare_arrival_time(const void* a, const void* b) {
-	return ((ThreadArgs*)a)->arrival_time - ((ThreadArgs*)b)->arrival_time;
-}
+HANDLE thread_handles[MAX_THREADS];
+DWORD thread_ids[MAX_THREADS];
 
-// FCFS funtion
-void fcfs(ThreadArgs thread_args[], int n) {
-	qsort(thread_args, n, sizeof(ThreadArgs), compare_arrival_time);
-	
-	int current_time = 0;
-    float total_waiting_time = 0, total_turnaround_time = 0;
-    printf("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        if (current_time < thread_args[i].arrival_time) {
-            current_time = thread_args[i].arrival_time;
+DWORD WINAPI FCFS(Thread* threads) {
+    float total_time = 0.0f;
+    float waiting_time = 0.0f;
+
+    for (int i = 0; i < MAX_THREADS; i++) {
+        if (total_time < threads[i].arrival_time) {
+            total_time = threads[i].arrival_time;
         }
-        int waiting_time = current_time - thread_args[i].arrival_time;
-        int turnaround_time = waiting_time + thread_args[i].burst_time;
-        total_waiting_time += waiting_time;
-        total_turnaround_time += turnaround_time;
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", thread_args[i].thread_id, thread_args[i].arrival_time, thread_args[i].burst_time, waiting_time, turnaround_time);
-        current_time += thread_args[i].burst_time;
-    }
-    printf("Average Waiting Time: %.2f\n", total_waiting_time / n);
-    printf("Average Turnaround Time: %.2f\n", total_turnaround_time / n);
-}
 
-// SJF function
-void sjf(ThreadArgs thread_args[], int n) {
-    // Sorting threads based on arrival time
-    qsort(thread_args, n, sizeof(ThreadArgs), compare_arrival_time);
-
-    int current_time = 0;
-    float total_waiting_time = 0, total_turnaround_time = 0;
-    printf("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        if (current_time < thread_args[i].arrival_time) {
-            current_time = thread_args[i].arrival_time;
-        }
-        int waiting_time = current_time - thread_args[i].arrival_time;
-        int turnaround_time = waiting_time + thread_args[i].burst_time;
-        total_waiting_time += waiting_time;
-        total_turnaround_time += turnaround_time;
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", thread_args[i].thread_id, thread_args[i].arrival_time, thread_args[i].burst_time, waiting_time, turnaround_time);
-        current_time += thread_args[i].burst_time;
-    }
-    printf("Average Waiting Time: %.2f\n", total_waiting_time / n);
-    printf("Average Turnaround Time: %.2f\n", total_turnaround_time / n);
-}
-
-// SRTF function
-void srtf(ThreadArgs thread_args[], int n) {
-    // Sorting threads based on arrival time
-    qsort(thread_args, n, sizeof(ThreadArgs), compare_arrival_time);
-
-    int current_time = 0, completed = 0;
-    float total_waiting_time = 0, total_turnaround_time = 0;
-    printf("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    while (completed < n) {
-        int shortest_index = -1, shortest_burst = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            if (thread_args[i].remaining_time > 0 && thread_args[i].arrival_time <= current_time && thread_args[i].burst_time < shortest_burst) {
-                shortest_index = i;
-                shortest_burst = thread_args[i].burst_time;
-            }
-        }
-        if (shortest_index == -1) {
-            current_time++;
-            continue;
-        }
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", thread_args[shortest_index].thread_id, thread_args[shortest_index].arrival_time, thread_args[shortest_index].burst_time, current_time - thread_args[shortest_index].arrival_time, current_time - thread_args[shortest_index].arrival_time + thread_args[shortest_index].burst_time);
-        total_waiting_time += current_time - thread_args[shortest_index].arrival_time;
-        total_turnaround_time += current_time - thread_args[shortest_index].arrival_time + thread_args[shortest_index].burst_time;
-        current_time += thread_args[shortest_index].burst_time;
-        thread_args[shortest_index].remaining_time = 0;
-        completed++;
-    }
-    printf("Average Waiting Time: %.2f\n", total_waiting_time / n);
-    printf("Average Turnaround Time: %.2f\n", total_turnaround_time / n);
-}
-
-// RR function
-void rr(ThreadArgs thread_args[], int n) {
-    // Sorting threads based on arrival time
-    qsort(thread_args, n, sizeof(ThreadArgs), compare_arrival_time);
-
-    int current_time = 0, completed = 0;
-    float total_waiting_time = 0, total_turnaround_time = 0;
-    printf("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    while (completed < n) {
-        for (int i = 0; i < n; i++) {
-            if (thread_args[i].remaining_time > 0 && thread_args[i].arrival_time <= current_time) {
-                int execution_time = (thread_args[i].remaining_time >= TIME_QUANTUM) ? TIME_QUANTUM : thread_args[i].remaining_time;
-                printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", thread_args[i].thread_id, thread_args[i].arrival_time, thread_args[i].burst_time, current_time - thread_args[i].arrival_time, current_time - thread_args[i].arrival_time + execution_time);
-                total_waiting_time += current_time - thread_args[i].arrival_time;
-                total_turnaround_time += current_time - thread_args[i].arrival_time + execution_time;
-                current_time += execution_time;
-                thread_args[i].remaining_time -= execution_time;
-                if (thread_args[i].remaining_time == 0) {
-                    completed++;
-                }
-            }
-        }
-    }
-    printf("Average Waiting Time: %.2f\n", total_waiting_time / n);
-    printf("Average Turnaround Time: %.2f\n", total_turnaround_time / n);
-}
-
-int main() {
-    // Thread parameters
-    ThreadArgs thread_args[NUM_THREADS];
-
-    // Getting arrival time and burst time from user input
-    printf("Enter arrival time and burst time for each thread:\n");
-    for (int i = 0; i < NUM_THREADS; i++) {
-        printf("Thread %d:\n", i + 1);
-        printf("Arrival time: ");
-        scanf_s("%d", &thread_args[i].arrival_time);
-        printf("Burst time: ");
-        scanf_s("%d", &thread_args[i].burst_time);
-        thread_args[i].thread_id = i + 1;
-        thread_args[i].remaining_time = thread_args[i].burst_time;
+        waiting_time += total_time - threads[i].arrival_time;
+        total_time += threads[i].execution_time;
     }
 
-    // Performing scheduling algorithms
-    printf("\nFirst-Come, First-Served (FCFS) Scheduling:\n");
-    fcfs(thread_args, NUM_THREADS);
-
-    printf("\nShortest Job First (SJF) Scheduling:\n");
-    sjf(thread_args, NUM_THREADS);
-
-    printf("\nShortest Remaining Time First (SRTF) Scheduling:\n");
-    srtf(thread_args, NUM_THREADS);
-
-    printf("\nRound Robin (RR) Scheduling:\n");
-    rr(thread_args, NUM_THREADS);
+    float avg_waiting_time = waiting_time / MAX_THREADS;
+    printf("Total time for FCFS: %.2f\n", total_time);
+    printf("Average waiting time for FCFS: %.2f\n", avg_waiting_time);
 
     return 0;
 }
 
+DWORD WINAPI SJF(Thread* threads) {
+    float total_time = 0.0f;
+    float waiting_time = 0.0f;
+    int executed[MAX_THREADS] = { 0 };
+
+    for (int i = 0; i < MAX_THREADS; i++) {
+        int shortest_thread = -1;
+        float shortest_time = FLT_MAX;
+
+        for (int j = 0; j < MAX_THREADS; j++) {
+            if (!executed[j] && threads[j].arrival_time <= total_time && threads[j].execution_time < shortest_time) {
+                shortest_thread = j;
+                shortest_time = threads[j].execution_time;
+            }
+        }
+
+        if (shortest_thread != -1) {
+            executed[shortest_thread] = 1;
+            total_time += threads[shortest_thread].execution_time;
+            waiting_time += total_time - threads[shortest_thread].arrival_time - threads[shortest_thread].execution_time;
+        }
+    }
+
+    float avg_waiting_time = waiting_time / MAX_THREADS;
+    printf("Total time for SJF: %.2f\n", total_time);
+    printf("Average waiting time for SJF: %.2f\n", avg_waiting_time);
+
+    return 0;
+}
+
+DWORD WINAPI SRTF(Thread* threads) {
+    float total_time = 0.0f;
+    float waiting_time = 0.0f;
+    int completed = 0;
+
+    while (completed < MAX_THREADS) {
+        float shortest_remaining_time = FLT_MAX;
+        int shortest_index = -1;
+
+        for (int i = 0; i < MAX_THREADS; i++) {
+            if (threads[i].remaining_time > 0.0f && threads[i].arrival_time <= total_time && threads[i].remaining_time < shortest_remaining_time) {
+                shortest_remaining_time = threads[i].remaining_time;
+                shortest_index = i;
+            }
+        }
+
+        if (shortest_index != -1) {
+            total_time++;
+            threads[shortest_index].remaining_time--;
+
+            if (threads[shortest_index].remaining_time == 0.0f) {
+                completed++;
+                waiting_time += total_time - threads[shortest_index].arrival_time - threads[shortest_index].execution_time;
+            }
+        }
+        else {
+            total_time++;
+        }
+    }
+
+    float avg_waiting_time = waiting_time / MAX_THREADS;
+    printf("Total time for SRTF: %.2f\n", total_time);
+    printf("Average waiting time for SRTF: %.2f\n", avg_waiting_time);
+
+    return 0;
+}
+
+DWORD WINAPI RR(Thread* threads) {
+    float total_time = 0.0f;
+    float waiting_time = 0.0f;
+    int current_thread = 0;
+    int completed_threads = 0;
+
+    while (completed_threads < MAX_THREADS) {
+        if (threads[current_thread].remaining_time > 0.0f) {
+            if (threads[current_thread].remaining_time > TIME_SLICE) {
+                total_time += TIME_SLICE;
+                threads[current_thread].remaining_time -= TIME_SLICE;
+            }
+            else {
+                total_time += threads[current_thread].remaining_time;
+                waiting_time += total_time - threads[current_thread].arrival_time - threads[current_thread].execution_time;
+                threads[current_thread].remaining_time = 0.0f;
+                completed_threads++;
+            }
+        }
+
+        current_thread = (current_thread + 1) % MAX_THREADS;
+    }
+
+    float avg_waiting_time = waiting_time / MAX_THREADS;
+    printf("Total time for RR: %.2f\n", total_time);
+    printf("Average waiting time for RR: %.2f\n", avg_waiting_time);
+
+    return 0;
+}
+
+int main() {
+    Thread threads[MAX_THREADS];
+
+    printf("Enter arrival and execution times for each thread:\n");
+    for (int i = 0; i < MAX_THREADS; i++) {
+        printf("Thread %d:\n", i + 1);
+        printf("Arrival time: ");
+        scanf_s("%f", &threads[i].arrival_time);
+        printf("Execution time: ");
+        scanf_s("%f", &threads[i].execution_time);
+        threads[i].thread_id = i + 1;
+        threads[i].remaining_time = threads[i].execution_time;
+    }
+
+    printf("\nFCFS\n");
+    thread_handles[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)FCFS, (LPVOID)threads, 0, &thread_ids[0]);
+    WaitForSingleObject(thread_handles[0], INFINITE);
+
+    printf("\nSJF\n");
+    thread_handles[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)SJF, (LPVOID)threads, 0, &thread_ids[1]);
+    WaitForSingleObject(thread_handles[1], INFINITE);
+
+    printf("\nSRTF\n");
+    thread_handles[2] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)SRTF, (LPVOID)threads, 0, &thread_ids[2]);
+    WaitForSingleObject(thread_handles[2], INFINITE);
+
+    printf("\nRR\n");
+    thread_handles[3] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)RR, (LPVOID)threads, 0, &thread_ids[3]);
+    Sleep(1000);
+    WaitForSingleObject(thread_handles[3], INFINITE);
+
+    for (int i = 0; i < MAX_THREADS; i++) {
+        CloseHandle(thread_handles[i]);
+    }
+
+    return 0;
+}
